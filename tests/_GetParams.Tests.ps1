fc366cc0a6b99a592a4d0bce0c794b1d829b77ba
Igor Abade <igoravl@gmail.com>
2026-03-17 02:03:46 -0300

    fix: remove obsolete tests for _GetParams

--- tests/_GetParams.Tests.ps1
+++ tests/_GetParams.Tests.ps1
@@ -5,37 +5,6 @@ BeforeAll {
     Import-Module $script:ModulePath -Force
 }
 
-Describe '_GetParams' {
-
-    Context 'When a $Map hashtable is provided' {
-
-        It 'Returns only entries whose value is not null' {
-            $result = InModuleScope PoshKelvin {
-                _GetParams -Map @{ search = 'foo'; name = $null; type = 'bar' }
-            }
-            $result.Keys | Should -Contain 'search'
-            $result.Keys | Should -Contain 'type'
-            $result.Keys | Should -Not -Contain 'name'
-        }
-
-        It 'Returns an empty hashtable when all map values are null' {
-            $result = InModuleScope PoshKelvin {
-                _GetParams -Map @{ search = $null; name = $null }
-            }
-            $result.Count | Should -Be 0
-        }
-
-        It 'Returns all non-null entries from the map' {
-            $result = InModuleScope PoshKelvin {
-                _GetParams -Map @{ a = '1'; b = '2' }
-            }
-            $result.Count | Should -Be 2
-            $result['a'] | Should -Be '1'
-            $result['b'] | Should -Be '2'
-        }
-    }
-}
-
 Describe '_GetParamName' {
 
     It 'Returns the lowercase parameter name when no aliases exist' {
